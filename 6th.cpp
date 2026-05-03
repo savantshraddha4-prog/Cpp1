9a80491b25aef0a53bcceec819e534a3e6a4504a
savantshraddha4-prog <savantshraddha4@gmail.com>
2026-05-03 21:20:48 +0530

    Add C++ program using friend function
    Implemented a C++ program demonstrating friend function usage to access private class members.

---
+++ 6th.cpp
@@ -0,0 +1,35 @@
+//Title:Friend Function
+//Problem statement: WAP C++ program to access private elements of class using friend function
+
+
+#include <iostream>
+using namespace std;
+
+class Sample
+{
+private:
+    int a, b;
+
+public:
+    Sample(int x, int y)
+    {
+        a = x;
+        b = y;
+    }
+
+    friend void showData(Sample s);
+};
+
+void showData(Sample s)
+{
+    cout << "Value of a = " << s.a << endl;
+    cout << "Value of b = " << s.b << endl;
+    cout << "Sum = " << (s.a + s.b) << endl;
+}
+
+int main()
+{
+    Sample obj(10, 20);
+    showData(obj);
+    return 0;
+}
