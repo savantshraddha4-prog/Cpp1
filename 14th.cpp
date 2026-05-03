50464ef29c65d628aa317ce3d156954d4b7a7649
savantshraddha4-prog <savantshraddha4@gmail.com>
2026-05-03 21:36:40 +0530

    Implement exception handling for division by zero

---
+++ 14th.cpp
@@ -0,0 +1,35 @@
+//Title: Exception handling
+//Problem statement: Arithmetic Exception Handling using single try and catch block (divisible
+//by 0)
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    float result;
+
+    cout << "Enter numerator: ";
+    cin >> num1;
+    cout << "Enter denominator: ";
+    cin >> num2;
+
+    try
+    {
+        if (num2 == 0)
+            throw "Error: Division by zero is not allowed!";
+        else
+        {
+            result = (float)num1 / num2;
+            cout << "Result = " << result << endl;
+        }
+    }
+    catch (const char *msg)
+    {
+        cout << msg << endl;
+    }
+
+    return 0;
+}
